b51990ecaf35dc343c0c5a70fec0ef11ea32ba8f
Glitch (hello-sqlite) <none>
2020-03-18 20:38:52 +0000

    👑😂 Checkpoint ./views/index.html:1690562/31 ./public/client.js:1690562/2055

--- public/client.js
+++ public/client.js
@@ -3,60 +3,17 @@
 
 console.log("hello world :o");
 const startJoinContainer = document.getElementById('start-or-join-container');
-const dreams = [];
+const startButton = document.getElementById('start-game');
+const joinButton = document.getElementById('join-game');
 
-// define variables that reference elements on our page
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements["dream"];
-const dreamsList = document.getElementById("dreams");
-const clearButton = document.querySelector('#clear-dreams');
 
-// request the dreams from our app's sqlite database
-fetch("/getDreams", {})
-  .then(res => res.json())
-  .then(response => {
-    response.forEach(row => {
-      appendNewDream(row.public_id);
-    });
-  });
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = dream => {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
-};
-
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = event => {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  const data = { dream: dreamInput.value };
-
-  fetch("/add-game", {
+startButton.addEventListener('click', (e) => {
+  fetch("/start-game", {
     method: "POST",
-    body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" }
   })
     .then(res => res.json())
     .then(response => {
       console.log(JSON.stringify(response));
     });
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form
-  dreamInput.value = "";
-  dreamInput.focus();
-};
-
-clearButton.addEventListener('click', event => {
-  fetch("/clear-games", {})
-    .then(res => res.json())
-    .then(response => {
-      console.log("cleared dreams");
-    });
-  dreamsList.innerHTML = "";
 });
