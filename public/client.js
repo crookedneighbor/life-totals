e3177cc0d80f189a69ea2880448487470c63a565
Glitch (hello-sqlite) <none>
2020-03-18 21:50:26 +0000

    🛥🎪 Checkpoint ./public/client.js:1690562/650 ./views/index.html:1690562/5981

--- public/client.js
+++ public/client.js
@@ -7,6 +7,7 @@ const gameContainer = document.getElementById('game-container');
 const startButton = document.getElementById('start-game');
 const joinButton = document.getElementById('join-game');
 const joinInput = document.getElementById('join-id');
+const addPlayerButton = document.getElementById('add-player');
 
 fetch('/get-games')
 
@@ -19,6 +20,8 @@ startButton.addEventListener('click', (e) => {
     .then(response => {
       if (response.success) {
         startJoinContainer.classList.add('is-hidden');
+        joinInput.value = response.publicId;
+        document.getElementById('game-code-container').classList.remove('is-hidden')
         document.getElementById('public-id').innerText = response.publicId;
         gameContainer.classList.remove('is-hidden');
       }
@@ -26,16 +29,25 @@ startButton.addEventListener('click', (e) => {
 });
 
 joinButton.addEventListener('click', (e) => {
+  startJoinContainer.classList.add('is-hidden');
+  gameContainer.classList.remove('is-hidden');
+});
+
+addPlayerButton.addEventListener('click', () => {
   fetch("/join-game", {
     method: "POST",
     body: JSON.stringify({
-      publicId: joinInput.value
+      publicId: joinInput.value,
+      name: document.getElementById('name-input').value
     }),
     headers: { "Content-Type": "application/json" }
   })
     .then(res => res.json())
     .then(response => {
+    console.log(response)
       if (response.success) {
+        document.getElementById('your-name').classList.add('is-hidden');
+        document.getElementById('your-life-total').classList.remove('is-hidden');
         startJoinContainer.classList.add('is-hidden');
         gameContainer.classList.remove('is-hidden');
       }
