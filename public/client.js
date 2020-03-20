5fc2fc61b25ed906e4a08504147b9f37c11a5f8e
Glitch (hello-sqlite) <none>
2020-03-20 16:22:36 +0000

    🍉🕰 Checkpoint ./public/client.js:1690562/85

--- public/client.js
+++ public/client.js
@@ -67,6 +67,11 @@ function createPlayer(name, life, color) {
       player.life = lifeTotal
       lifeTotalContainer.innerText = lifeTotal
     },
+    updateLife() {
+      player.update({
+        life: player.life
+      })
+    },
     update(updates) {
       clearTimeout(updateTimeoutRef);
       player.updateInProgress = true
